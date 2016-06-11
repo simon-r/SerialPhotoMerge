2ffbde222c4b5d6d85f1194a72fdb2634070d8c4
Simone Riva <simone.rva@gmail.com>
2016-06-11 18:37:10 +0200

    Completed forst version of ReadImageFarctory

--- imgmerge/readimgfactory.py
+++ imgmerge/readimgfactory.py
@@ -18,8 +18,35 @@ from imgmerge.readimg import ReadImageBasic, ReadImageRaw
 
 class ReadImageFarctory( object ):
     def __init__(self):
-        _img_reads = [ ReadImageBasic() , ReadImageRaw() ]
+        self._img_reads = [ ReadImageBasic() , ReadImageRaw() ]
+        self._default_reader = self._img_reads[0]
+        self._force_dafault = False
+
+    def get_default_reader(self):
+        return self._default_reader
+
+    def set_default_reader(self, def_r):
+        self._default_reader = def_r
+
+    default_reader = property(get_default_reader, set_default_reader)
+
+    def get_force_default(self):
+        return self._force_dafault
+
+    def set_force_dafault(self,f):
+        self._force_dafault = f 
+
+    force_default = property(get_force_default, set_force_dafault)
+
+    def get_readimage(self, file_name=None):
+        if not file_name or self.force_default:
+            return self._default_reader
+
+        for reader in self._img_reads :
+            if reader.is_supported( file_name ):
+                return reader
+
+        return None
+
     
-    def get_readimage(self):
-        pass
     
