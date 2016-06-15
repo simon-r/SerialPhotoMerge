7a150c51b337dff5ad412b774791448462717acc
Simone Riva <simone.rva@gmail.com>
2016-06-15 22:25:23 +0200

    Some works on WriteImageFactory

--- imgmerge/writeimagefactory.py
+++ imgmerge/writeimagefactory.py
@@ -14,10 +14,12 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from imgmerge.writeimg import *
 
 class WriteImageFactory( object ):
     def __init__(self):
         self._default_wi = None
+        self._chosen_wi = None
 
     def force_default(self, wi):
         self._default_wi = wi
@@ -25,8 +27,14 @@ class WriteImageFactory( object ):
     def remove_default(self):
         self._default_wi = None
 
-    def set_image_parameters(self, type="jpg", color_depth=8):
-        pass
+    def set_image_parameters(self, format="jpg", color_depth=8, lossy=True, **kwargs ):
+        if format in [ "jpg", "jpeg" ]:
+            self._chosen_wi = WriteImageBasic()
+            return 
+        elif format in [ "png" ]:
+            pass
+        elif format in [ "tif", "tiff" ]:
+            pass
     
     def get_write_image(self):
         if self._default_wi :
