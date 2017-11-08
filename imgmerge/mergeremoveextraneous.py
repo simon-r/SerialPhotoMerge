bb5cb0c87e2408fa04185741b5474cb65c6bd7d7
Simone Riva <simone.rva@gmail.com>
2017-11-08 21:55:02 +0100

    Working on CUDA remove extraneous

--- imgmerge/mergeremoveextraneous.py
+++ imgmerge/mergeremoveextraneous.py
@@ -20,6 +20,12 @@ import numpy as np
 import sys
 import time
 
+try:
+    import pycuda.autoinit
+    from pycuda import gpuarray
+except:
+    pass
+
 import scipy.ndimage as ndimage
 from imgmerge.readimg import ReadImageBasic
 from imgmerge.image import Image
@@ -90,3 +96,56 @@ class MergeRemoveExtraneous(MergeProcedureVirtual):
             avrimg.image[:] = 0.0
 
         self.resulting_image = resimg
+
+
+class MergeRemoveExtraneousCUDA(MergeProcedureVirtual):
+
+    def __init__(self):
+        super().__init__()
+
+    def __init_kernel_dist_colors(self):
+        self.__kernel_dist_colors =
+        """
+        __global__ void dist_colors(float *imgarr, float *avrimg, float w=10.0)
+        {
+            const int i = threadIdx.x;
+            dest[i] = a[i] * b[i];
+        }
+        """
+
+    def execute(self):
+        self.resulting_image = None
+        f_first = True
+
+        resimg = self.images_iterator.read_reference_image()
+        shape = resimg.shape
+
+        resimg.image[:] = 2**resimg.color_depth / 2
+
+        resimg_nda = np.ndarray(shape=resimg.image.shape,
+                                dtype=resimg.image.dtype)
+        resimg_nda[:] = resimg.image[:]
+
+        resimg_cu = gpuarray.to_gpu(resimg_nda)
+        imgarr_cu = gpuarray.to_gpu(resimg_nda)
+
+        avrimg_cu = gpuarray.zeros_like(resimg_cu)
+        std_cu = gpuarray.zeros(
+            shape[:2], dtype=resimg.dtype) + 2**resimg.color_depth
+
+        dist_cu = gpuarray.zeros(shape[:2], dtype=resimg.dtype)
+        flags_cu = gpuarray.zeros(shape[:2], dtype=np.bool)
+
+        iter_cnt = 5
+
+        for itr in range(iter_cnt):
+            invalid_imgs = []
+            img_cnt = 0.0
+
+            for imgarr in self.images_iterator:
+
+                if shape != imgarr.shape:
+                    self.images_iterator.discard_image()
+                    continue
+
+                imgarr_cu.set(imgarr.image)
